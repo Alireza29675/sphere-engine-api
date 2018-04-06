3e3fb9f067b9a4ace036e90f346664d17dc7f881
Alireza29675 <alireza.sheikholmolouki@gmail.com>
2018-04-07 01:22:05 +0430

    making index.js empty

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 const Sphere = require('./lib')
 
-const sphere = new Sphere({ token: '' });
-
-sphere.compile({
-    language: 'c++',
-    source: '#include <stdio.h>\n int main(){ // some code }'
-}).then(result => {
-    console.log(result);
-});
-
 module.exports = Sphere;
