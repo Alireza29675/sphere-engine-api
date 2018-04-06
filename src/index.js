f951cea7e7c80972e6f92ebcb50110357290a66a
Alireza29675 <alireza.sheikholmolouki@gmail.com>
2018-04-06 23:39:12 +0430

    Sphere class has been created

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-export default function sphereEngine() {
+export default class Sphere {
+
+    /**
+     * Represents a SphereEngine Compiler
+     * @constructor
+     * @param 
+     */
+    constructor (options) {
+        this.compilerToken = options.token
+    }
+
+    /**
+     * Requests for a Sphere Compile
+     * @param {object} options
+     */
+    compile (options = { language: 'python', source: '' }) {
+
+    }
 
 }
